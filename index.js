2b75572b16c2dd986b875e2859738380e96740e1
onurkybsi <onurbpm@outlook.com>
2020-04-22 01:54:18 +0300

    Commit4

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 $("document").ready(function () {
-  var toDoList = $("#toDoList").html();
-
   $("#addTodo").click(function () {
     if ($("#toBeAdded").val().trim()) {
-      toDoList += `
-        <li class="ui-state-default">
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" value="" />
-                    ${$("#toBeAdded").val()}
-                </label>
-            </div>
-        </li>
-            `;
-      $("#toDoList").html(toDoList);
+      $("#toDoList").append(`
+      <li class="ui-state-default">
+          <div class="checkbox">
+              <label>
+                  <input type="checkbox" value="" />
+                  ${$("#toBeAdded").val()}
+              </label>
+          </div>
+      </li>
+          `);
     }
   });
 });
